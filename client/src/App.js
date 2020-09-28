eb230b262f4cb1f44eac86ead95ab3a126c01b11
Shubham Negi <shubham.n@trgint.com>
2020-09-28 19:24:30 +0530

    Starter code, running client and server concurrently

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Fellow coders!!! Lets start building our app.
+    </div>
+  );
+}
+
+export default App;
